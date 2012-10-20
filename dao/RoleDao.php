67ee1ea532f3b8564c9cef82b37e716700d172f5
Jean-Charles BAILLIEZ <jc.bailliez@gmail.com>
2012-10-20 17:48:06 +0200

    daorole

--- dao/RoleDao.php
+++ dao/RoleDao.php
@@ -2,7 +2,7 @@
 require_once '../model/Role.php';
 
 function getRoleById($id){
-    $roleSQL = self::$connexion->ExecuteSelect('SELECT * FROM role WHERE IDROLE = '.$id);
+    $roleSQL = self::$connexion->ExecuteSelectOne('SELECT * FROM role WHERE IDROLE = '.$id);
     $role = new Role($roleSQL['IDROLE'], $roleSQL['LBLROLE'], $roleSQL['TYPEROLE'], $roleSQL['IMAGEROLE']);
         
 	return $role;
