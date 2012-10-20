36daf51e3fdf1f4ba4df586c27fe44b600ffff12
Laurent Soyris <Soyris@gmail.com>
2012-10-21 01:31:29 +0200

    page d'accueil pour test

--- model/dbConnect.php
+++ model/dbConnect.php
@@ -40,8 +40,8 @@ class dbConnect {
     
     public static function getInstance() {
         if(is_null(self::$connexion)) {
-            // self::$connexion = new dbConnect('localhost', 'comulien', 'root', '');
-        	self::$connexion = new dbConnect('localhost', 'hackaton', 'hackaton', 'hackaton');
+             self::$connexion = new dbConnect('localhost', 'cormulien', 'root', 'root');
+        //	self::$connexion = new dbConnect('localhost', 'hackaton', 'hackaton', 'hackaton');
         }
         return self::$connexion;
     }
