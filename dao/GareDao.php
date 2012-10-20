6670afa67f7af7bd61d0c0864291352621b94351
TheManu <tempo05@laposte.net>
2012-10-20 16:07:37 +0200

    gareDAO v2

--- dao/GareDao.php
+++ dao/GareDao.php
@@ -1,12 +1,21 @@
 ﻿<?php
+
+include_once('../model/gare.php');
 class GareDAO{
-    public function getGareById($id){
-        $gare = self::$connexion->ExecuteSelect('SELECT * FROM gare WHERE IDGARE=' . $id );
-        return $gare;
-    }
+    //public function getGareById($id){
+        //$gareSQL = self::$connexion->ExecuteSelect('SELECT * FROM gare WHERE IDGARE=' . $id );
+        //....
+        //return $gare;
+    //}
     
     public function getGares($id){
-        $gares = self::$connexion->ExecuteSelect('SELECT * FROM gare');
+        $garesSQL = self::$connexion->ExecuteSelect('SELECT * FROM gare');
+        $gares = array();
+        foreach($garesSQL as $gareSQL)
+        {
+            $gares[] = new Gare($gareSQL['IDGARE'], $gareSQL['NOMGARE'])
+        }
+        
         return $gares;
     }
 }
