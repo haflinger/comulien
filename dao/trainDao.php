ce5440e6a66ad5ef55342b655859c6a8f7d910b4
TheManu <tempo05@laposte.net>
2012-10-20 16:32:31 +0200

    trainDAO

--- dao/trainDao.php
+++ dao/trainDao.php
@@ -11,7 +11,25 @@
  * @author Fred
  */
 class trainDao {
-    //put your code here
+    
+    public function getTrainById($id){
+        $trainSQL = self::$connexion->ExecuteSelect('SELECT * FROM train WHERE IDTRAIN = ' . $id);
+        $train = new train($trainSQL['IDTRAIN'], $trainSQL['NOTRAIN']);
+        
+        return $train;
+    }
+    
+    public function getTrainByName($name){
+        $trainSQL = self::$connexion->ExecuteSelect('SELECT * FROM train WHERE NOTRAIN = ' . $name);
+        $train = new train($trainSQL['IDTRAIN'], $trainSQL['NOTRAIN']);
+        
+        return $train;
+    }
+    
+    public function getTrainsPourGare($idGare)
+    {
+        //todo.......
+    }
 }
 
 ?>
