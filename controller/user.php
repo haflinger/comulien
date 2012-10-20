08965a5028609fa54af3489778a0daee8de2470c
Jean-Charles BAILLIEZ <jc.bailliez@gmail.com>
2012-10-20 14:40:36 +0200

    v1

---
+++ controller/user.php
@@ -0,0 +1,29 @@
+<?php
+
+class User {
+	
+	// Definition des variables
+	
+	// Id de l'utilisateur
+	var $id_user; 
+	
+	// Login de l'utilisateur
+	var $login;
+	
+	// Password de l'utilisateur
+	var $password;
+	
+	// Email de l'utilisateur
+	var $email;
+	
+	// Nombre de messages
+	var $nb_message;
+	
+	// Nombre de plus
+	var $nb_ok;
+	
+	// Date de l'inscription
+	var $date_inscription;
+}
+
+?>
