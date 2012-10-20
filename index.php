d583182d8927c69ef1be8b6e5b831d8e99d834fc
Pieter Colpaert <pieterc@member.fsf.org>
2012-10-20 12:50:15 +0200

    Added a dossier for the view. Don't be shocked when your project is not working anymore, just go to the view/ directory.

--- index.php
+++ index.php
@@ -1,34 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="utf-8">
-        <title>Page Title</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="assets/css/bootstrap.css" rel="stylesheet">
-        <link href="assets/css/bootstrap-responsive.css" rel="stylesheet">
-    </head>
-    <body>
-        <div class="hero-unit">
-            <h1>Heading</h1>
-            <p>Tagline</p>
-            <p>
-                <a class="btn btn-primary btn-large">
-                    Learn more
-                </a>
-            </p>
-        </div>
-    </body>
-    <script src="assets/js/jquery.js"></script>
-    <script src="assets/js/bootstrap-transition.js"></script>
-    <script src="assets/js/bootstrap-alert.js"></script>
-    <script src="assets/js/bootstrap-modal.js"></script>
-    <script src="assets/js/bootstrap-dropdown.js"></script>
-    <script src="assets/js/bootstrap-scrollspy.js"></script>
-    <script src="assets/js/bootstrap-tab.js"></script>
-    <script src="assets/js/bootstrap-tooltip.js"></script>
-    <script src="assets/js/bootstrap-popover.js"></script>
-    <script src="assets/js/bootstrap-button.js"></script>
-    <script src="assets/js/bootstrap-collapse.js"></script>
-    <script src="assets/js/bootstrap-carousel.js"></script>
-    <script src="assets/js/bootstrap-typeahead.js"></script>
-</html>
+<?php
+//Added this as a temporary solution to make the link with view.
+
+//Over here the "router" should be written: a file which checks every URI and redirects it to the right Controller (in the MVC model)
+
+include_once("view/index.php");
