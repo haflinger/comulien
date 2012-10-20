03dd8d3bad91542545033f4bb2d9518bf7734d03
FredoHorn <fnibh8re>
2012-10-20 16:12:40 +0200

    message et train

---
+++ dao/messageDao.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of messageDao
+ *
+ * @author Fred
+ */
+class messageDao {
+    //put your code here
+}
+
+?>
---
+++ dao/trainDao.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of trainDao
+ *
+ * @author Fred
+ */
+class trainDao {
+    //put your code here
+}
+
+?>
