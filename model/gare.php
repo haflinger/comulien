dd9613de77c7f7bc9439f558039d3c56cb501dbb
TheManu <tempo05@laposte.net>
2012-10-20 15:33:49 +0200

    gareDTO

---
+++ model/gare.php
@@ -0,0 +1,30 @@
+﻿<?php
+
+class gare {
+    var $id_gare;
+    var $nom_gare;
+       
+    private function __construct($id, $nom) {
+        $this->$id_gare = $id;
+        $this->$nom_gare = $nom;
+    }
+    
+	
+	function getId(){
+		return $this->$id_gare;
+	}
+	
+	function setId($id){
+		$this->$id_gare = $id;
+	}
+	
+	function getNom(){
+		return $this->$nom_gare;
+	}
+	
+	function setgetNom($nom){
+		$this->$nom_gare = $nom;
+	}
+}
+
+?>
