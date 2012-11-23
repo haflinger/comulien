37f1986e129d45d27e267a76a9b5e2f86b37aa9a
alexsolex <alexsolex@gmail.com>
2012-11-23 23:17:08 +0100

    création du controleur, du modèle, et de la vue, pour l'entité Utilisateur

---
+++ application/models/DbTable/Utilisateur.php
@@ -0,0 +1,19 @@
+<?php
+
+
+/**
+ * Description of Utilisateur
+ *
+ * @author alexsolex
+ */
+class Application_Model_DbTable_Utilisateur extends Zend_Db_Table_Abstract
+{
+    protected $_name = 'Utilisateur';
+    protected $_primary = 'IDUSER';
+//    public function insert(array $data)
+//        {
+//            return parent::insert($data);
+//        }
+}
+
+?>
