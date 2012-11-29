d7e0cf216c4d67e8d95dc44ddd8bd9a2a91e2512
Fredo <fredo.ndm@gmail.com>
2012-11-29 20:09:26 +0100

    Rectification du bootstrap pour l'UTF-8

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -7,6 +7,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         $this->bootstrap('view');
         $view = $this->getResource('view');
         $view->doctype('XHTML1_STRICT');
+        $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8');
     }
 
 }
