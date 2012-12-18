8a7594b381d037a94a6ba271c2bf49d4279ad759
alexsolex <alexsolex@gmail.com>
2012-12-18 15:46:53 +0100

    organisation du controller 'evenement'

--- application/controllers/EvenementController.php
+++ application/controllers/EvenementController.php
@@ -17,15 +17,46 @@ class EvenementController extends Zend_Controller_Action
      * Connexion à un événement par une URL, un QrCode....
      * C'est le point d'entrée de l'application !
      */
+    
+    /**
+     * index : gère la situation lors de l'arrivée dans l'évènement
+     * par défaut : affiche l'accueil de l'évènement
+     */
     public function indexAction()
+    {
+        $this->_helper->redirector ( 'accueil', 'evenement' );
+    }
+    
+    public function accueilAction()
+    {
+        $defaultNamespace = new Zend_Session_Namespace('bulle');
+        //session active ?
+        if (!isset($defaultNamespace->checkedInEvent)) {
+            $this->_helper->redirector ( 'liste', 'evenement' );
+        }
+        else
+        {
+            $this->view->evenement = $defaultNamespace->checkedInEvent;
+        }
+        
+        
+        
+    }
+    
+    public function listeAction()
     {
         //Connexion à un événement
         $Evenement = new Application_Model_DbTable_Evenement();
         $this->view->entries = $Evenement->fetchAll();
     }
     
+    /**
+     * checkin est une action purement technique qui va juste procéder au test
+     * du checkin dans l'évènement et à la persistance en session de l'évènement checké
+     */
     public function checkinAction()
     {
+        
         // site/evenement/checkin/id/1
         // ou
         // site/evenement/checkin/1
@@ -38,20 +69,24 @@ class EvenementController extends Zend_Controller_Action
             //TODO tester la validité de l'évènement
             //...
             //Sauve l'évènement dans la session
-            $defaultNamespace = new Zend_Session_Namespace('Comulien');
+            $defaultNamespace = new Zend_Session_Namespace('bulle');
             $defaultNamespace->checkedInEvent = $eventDeID;
             
-            $this->view->evenement = $eventDeID;
+            //redirection sur l'accueil de l'évènement
+            $this->_helper->redirector ( 'accueil', 'evenement' );
             
         } else { 
-            $this->view->evenement = null;
+            //redirection sur une autre action de délestage
+            //(temporairement sur la liste des évènements)
+            
+            $this->_helper->redirector ( 'liste', 'evenement' );
             
         }
     }
     
     public function checkoutAction()
     {
-        $defaultNamespace = new Zend_Session_Namespace('Comulien');
+        $defaultNamespace = new Zend_Session_Namespace('bulle');
         unset($defaultNamespace->checkedInEvent);
         $this->_helper->redirector ( 'index', 'index' );
     }
--- application/views/helpers/AccueilEventLink.php
+++ application/views/helpers/AccueilEventLink.php
@@ -1,22 +1,22 @@
 <?php
 /**
- * Permet de générer le lien de connexion / déconnexion
+ * Permet de générer le lien de checkout / checkin dans un évènement
  */
 class Zend_View_Helper_AccueilEventLink extends Zend_View_Helper_Abstract {
     
     public function accueilEventLink() {
         $helperUrl = new Zend_View_Helper_Url ( );
-        $comulienNamespace = new Zend_Session_Namespace('Comulien');
+        $comulienNamespace = new Zend_Session_Namespace('bulle');
         if (isset($comulienNamespace->checkedInEvent))
         {
             $event = $comulienNamespace->checkedInEvent;
-            $accueilLink = $helperUrl->url ( array ('action' => 'index', 'controller' => 'evenement' ) );
+            $accueilLink = $helperUrl->url ( array ('action' => 'accueil', 'controller' => 'evenement' ) );
             $checkoutLink = $helperUrl->url ( array ('action' => 'checkout', 'controller' => 'evenement' ) );
-            return '<a href="'.$accueilLink.'" alt="Accueil Evenement">'.$event->titreEvent.'</a> | <a href="'.$checkoutLink.'" alt="checkout">checkout</a>';
+            return '<a href="'.$accueilLink.'" alt="Accueil Evenement">'.'Accueil '.$event->titreEvent.'</a> | <a href="'.$checkoutLink.'" alt="checkout">checkout</a>';
         }
         else
         {
-            $accueilLink = $helperUrl->url ( array ('action' => 'index', 'controller' => 'evenement' ) );
+            $accueilLink = $helperUrl->url ( array ('action' => 'liste', 'controller' => 'evenement' ) );
             return 'Vous n\'êtes pas dans un évènement <a href="'.$accueilLink.'" alt="Accueil Evenement">Liste des évènements</a>';
         }
         
