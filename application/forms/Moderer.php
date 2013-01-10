ff424036910d38bcf6e84a254be363408bcc2485
alexsolex <alexsolex@gmail.com>
2013-01-10 23:07:05 +0100

    Le view helper de moderation a été transformé en formulaire

---
+++ application/forms/Moderer.php
@@ -0,0 +1,33 @@
+<?php
+
+class Application_Form_Moderer extends Zend_Form
+{
+
+    private $idMessage;
+
+    public function init()
+    {
+        
+    }
+    
+    public function setIdMessage($idMessage){
+        $this->idMessage = $idMessage;
+    }
+    
+    public function genererForm(){
+        $hiddenIdMessage = new Zend_Form_Element_Hidden('hiddenIdMessage');
+        $hiddenIdMessage->setValue($this->idMessage);
+            
+        $this->setAction('moderer')->setMethod('post');
+        $this->setName ( 'moderer_message_'.$this->idMessage);
+        
+        $submit = new Zend_Form_Element_Submit ( 'submit'.$this->idMessage );
+        $submit->setAttrib ( 'id', 'submitbutton'.$this->idMessage )->setLabel ( 'moderer'.$this->idMessage );
+
+        $elements = array ($hiddenIdMessage, $submit );
+        $this->addElements ( $elements );
+    }
+   
+
+}
+
