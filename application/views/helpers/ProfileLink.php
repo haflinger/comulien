05033d4e7e993f939b61bcc15b9c7220b8a57ee1
Fredo <fredo.ndm@gmail.com>
2013-01-11 03:32:00 +0100

    Ajout d'un header, d'un footer et modification des helpers

--- application/views/helpers/ProfileLink.php
+++ application/views/helpers/ProfileLink.php
@@ -10,10 +10,13 @@ class Zend_View_Helper_ProfileLink extends Zend_View_Helper_Abstract {
             $username = $auth->getIdentity ()->loginUser;// . ' ' . strtoupper ( substr ( $auth->getIdentity ()->loginUser, 0, 1 ) ) . '.';
             $lienDeconnexion = $helperUrl->url ( array ('action' => 'deconnecter', 'controller' => 'utilisateur' ) );
             $lienProfil = $helperUrl->url ( array ('action' => 'modifier', 'controller' => 'utilisateur' ) );
-            $retourHelper ='<a href="'.$lienProfil.'" alt="Voir mon profil">'.$username . '</a> (<a href="' . $lienDeconnexion . '">Logout</a>)';
+            //$retourHelper ='<a href="'.$lienProfil.'" alt="Voir mon profil">'.$username . '</a> (<a href="' . $lienDeconnexion . '">Logout</a>)';
+            $retourHelper = $lienDeconnexion;
+            
         }else{
             $lienConnexion = $helperUrl->url ( array ('action' => 'authentifier', 'controller' => 'utilisateur' ) );
-            $retourHelper = '<a href="' . $lienConnexion . '">Login</a>';
+            //$retourHelper = '<a href="' . $lienConnexion . '">Login</a>';
+            $retourHelper = $lienConnexion;
         }
         return $retourHelper;
     }
