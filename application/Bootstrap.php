3e9d7cd740a1a232ef58eb85c411aa46b7c95b6f
alexsolex <alexsolex@gmail.com>
2013-01-20 01:26:38 +0100

    ajout de ZFDebug qui permet d'obtenir quelques infos bien utiles pour le débug de Zend http://code.google.com/p/zfdebug/wiki/Installation

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -39,5 +39,42 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
         Zend_Controller_Front::getInstance()->registerPlugin(new Application_Plugin_PluginAuth($acl));
         Zend_Registry::set('Zend_Acl', $acl);
     }
+    
+
+    protected function _initZFDebug()
+    {
+        $autoloader = Zend_Loader_Autoloader::getInstance();
+        $autoloader->registerNamespace('ZFDebug');
+
+        $options = array(
+            'plugins' => array('Variables', 
+                               'File' => array('base_path' => '/path/to/project'),
+                               'Memory', 
+                               'Time', 
+                               'Registry', 
+                               'Exception')
+        );
+
+        # Instantiate the database adapter and setup the plugin.
+        # Alternatively just add the plugin like above and rely on the autodiscovery feature.
+        if ($this->hasPluginResource('db')) {
+            $this->bootstrap('db');
+            $db = $this->getPluginResource('db')->getDbAdapter();
+            $options['plugins']['Database']['adapter'] = $db;
+        }
+
+        # Setup the cache plugin
+        if ($this->hasPluginResource('cache')) {
+            $this->bootstrap('cache');
+            $cache = $this-getPluginResource('cache')->getDbAdapter();
+            $options['plugins']['Cache']['backend'] = $cache->getBackend();
+        }
+
+        $debug = new ZFDebug_Controller_Plugin_Debug($options);
+
+        $this->bootstrap('frontController');
+        $frontController = $this->getResource('frontController');
+        $frontController->registerPlugin($debug);
+    }
 }
 
