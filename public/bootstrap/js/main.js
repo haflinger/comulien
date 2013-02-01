f74e5abbb21859f766038861c752e18d142c5e37
Fredo <fredo.ndm@gmail.com>
2013-02-01 17:25:11 +0100

    rectif du test de l'information evenement

--- public/bootstrap/js/main.js
+++ public/bootstrap/js/main.js
@@ -1,18 +1,16 @@
 $(document).ready(function() {
-//au click sur le lien chercher
+//au click sur le lien evenement
 $("#nomEvent").click(function(){
-//on recupere la valeur de l'attribut name pour afficher tel ou tel resultat
-//requête ajax, appel du fichier recherche.php
 $.ajax({
 type: "GET",
 url: BASE_URL + "/evenement/accueil",
 dataType : "html",
 //affichage de l'erreur en cas de problème
-error:function(msg, string){
+error:function(string){
 alert( "Error !: " + string );
 },
+
 success:function(data){
-//alert(data);
 //on met à jour le div zone_de_rechargement avec les données reçus
 //on vide la div et on le cache
 $("#detailsEvent").empty().hide();
