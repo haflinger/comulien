e4f3fda33fe4367c19dc4e72eb90453faaccfeb3
Fredo <fredo.ndm@gmail.com>
2013-02-05 06:41:04 +0100

    jquery suite

--- public/bootstrap/js/main.js
+++ public/bootstrap/js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
 //au click sur le lien evenement
     $("#monaccordeon").click(function(){
+        console.log( $("#IdMessageParent"));
+        var valeur = $("#IdMessageParent").val();
         $.ajax({
             type: "GET",
-            url: BASE_URL + "/message/reponses",
+            url: BASE_URL + "/message/reponses/message/"+ valeur +"?format=json",
             dataType : "html",
             //affichage de l'erreur en cas de problème
             error:function(string){
@@ -13,11 +15,11 @@ $(document).ready(function() {
             success:function(data){
                 //on met à jour le div zone_de_rechargement avec les données reçus
                 //on vide la div et on le cache
-                $("#monaccordeonMenu").empty().hide();
+                $("#reponses"+ valeur).empty().hide();
                 //on affecte les resultats au div
-                $("#monaccordeonMenu").append(data);
+                $("#reponses"+ valeur).append(data);
                 //on affiche les resultats avec la transition
-                $('#monaccordeonMenu').fadeIn(1000);
+                $("#reponses"+ valeur).fadeIn(1000);
             }
         });
     });
