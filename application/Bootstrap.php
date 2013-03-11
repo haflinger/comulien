3212365f5911b9270f240df779d41cac7ddb7c4d
alexsolex <alexsolex@gmail.com>
2013-03-11 18:19:09 +0100

    doctype HTML5 dans le Bootstrap

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -9,7 +9,7 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     {
         $this->bootstrap('view');
         $view = $this->getResource('view');
-        $view->doctype('XHTML1_STRICT');
+        $view->doctype('HTML5');
         $view->addHelperPath(APPLICATION_PATH . '/views/helpers');
         $view->headMeta()->appendHttpEquiv('Content-Type', 'text/html;charset=utf-8');
     }
